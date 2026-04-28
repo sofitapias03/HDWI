616d606aa0026d85e0d9c300e67207c8edb197ee
sofia tapias <sofia.tapiasmontana@ucalgary.ca>
2026-04-27 20:56:15 -0600

    changes router

--- hdwi-website/src/App.js
+++ hdwi-website/src/App.js
@@ -1,10 +1,26 @@
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Landing from './pages/Landing';
+
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Landing />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/HDWI">
       <Routes>
         <Route path="/" element={<Landing />} />
       </Routes>
