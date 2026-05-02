15815b3f227a6214e741aee71fdfe78e6a92af9a
sofia tapias <sofia.tapiasmontana@ucalgary.ca>
2026-05-02 11:13:52 -0600

    change tab picture

--- hdwi-website/src/App.js
+++ hdwi-website/src/App.js
@@ -1,19 +1,3 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Landing from './pages/Landing';
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<Landing />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
