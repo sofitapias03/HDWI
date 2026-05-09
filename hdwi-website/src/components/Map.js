c855dd92949528978a882214e1fec6a313aca96f
sofia tapias <sofia.tapiasmontana@ucalgary.ca>
2026-05-08 22:32:01 -0600

    updated the map component and fixed the useState

--- hdwi-website/src/components/Map.js
+++ hdwi-website/src/components/Map.js
@@ -4,19 +4,17 @@ import fallBackMap from '../images/fall-back-map.png';
 
 function Map({ day }) {
   const runDate = new Date().toISOString().slice(0, 10).replace(/-/g, "");
-  const [imgSrc, setImgSrc] = useState (
-    `https://raw.githubusercontent.com/sofitapias03/HDWI/pulling/maps/maxMap_day_${day}.png?v=${runDate}`
-  );
+  const [error, setError] = useState(false);
 
   return (
     <div className="map-wrapper">
       <img
-          src={imgSrc}
+          src={error ? fallBackMap : `https://raw.githubusercontent.com/sofitapias03/HDWI/pulling/maps/maxMap_day_${day}.png?v=${runDate}`}
           alt="Forecast map"
           className="map-image"
           onError={ (e) => {
             e.target.onerror = null;
-            setImgSrc(fallBackMap)
+            setError(true);
           }}
       />
     </div>
